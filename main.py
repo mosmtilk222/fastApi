30a4356e567fb3cf209c272119a2d579b9b66ead
mosmtilk222 <acuetopea@gmail.com>
2022-01-22 09:19:20 -0600

    models tweets

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
+#Python
+from uuid import UUID
+from datetime import date, datetime
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+#FastAPI
 from fastapi import FastAPI
+
+#Uvicorn
 import uvicorn
 
 app = FastAPI()
 
+#Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
+
+
+class Match(BaseModel):
+  pass
+
 @app.get(path="/")
 def home():
-  return {"Twitter APi": "Working"}
+  return {"Twitter API": "Working"}
 
 if __name__ == '__main__':
   uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
