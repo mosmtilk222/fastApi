695e331b01a4dcca782431483c9ae2e04623ab76
mosmtilk222 <acuetopea@gmail.com>
2022-01-22 09:37:05 -0600

    path_ops_structure_users

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Python
 from uuid import UUID
 from datetime import date, datetime
-from typing import Optional
+from typing import Optional, List
 
 #Pydantic
 from pydantic import BaseModel
@@ -10,6 +10,7 @@ from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 #Uvicorn
 import uvicorn
@@ -52,13 +53,77 @@ class Tweet(BaseModel):
     update_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
-
-class Match(BaseModel):
-  pass
+#Paths
 
 @app.get(path="/")
 def home():
   return {"Twitter API": "Working"}
+
+##Users
+
+@app.post(
+  path="/signup",
+  response_model=User,
+  status_code=status.HTTP_201_CREATED,
+  summary="Register a User",
+  tags=["Users"]
+)
+def signup():
+  pass
+
+@app.post(
+  path="/login",
+  response_model=User,
+  status_code=status.HTTP_200_OK,
+  summary="Login a User",
+  tags=["Users"]
+)
+def login():
+  pass
+
+@app.get(
+  path="/users",
+  response_model=List[User],
+  status_code=status.HTTP_200_OK,
+  summary="Show all users",
+  tags=["Users"]
+)
+def show_all_users():
+  pass
+
+@app.get(
+  path="/users/{user_id}",
+  response_model=User,
+  status_code=status.HTTP_200_OK,
+  summary="Show a User",
+  tags=["Users"]
+)
+def show_a_user():
+  pass
+
+@app.delete(
+  path="/users/{user_id}/delete",
+  response_model=User,
+  status_code=status.HTTP_200_OK,
+  summary="Delete a User",
+  tags=["Users"]
+)
+def delete_a_user():
+  pass
+
+@app.put(
+  path="/users/{user_id}/update",
+  response_model=User,
+  status_code=status.HTTP_200_OK,
+  summary="Update a User",
+  tags=["Users"]
+)
+def update_a_user():
+  pass
+
+##Tweets
+
+
 
 if __name__ == '__main__':
   uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
