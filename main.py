27300c00929ce1a91a18b8b1ed27fd6a835cc655
mosmtilk222 <acuetopea@gmail.com>
2022-01-23 15:45:49 -0600

    tweet_post_logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Python
+import json
 from urllib import response
 from uuid import UUID
 from datetime import date, datetime
@@ -12,6 +13,7 @@ from pydantic import Field
 #FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import Body
 
 #Uvicorn
 import uvicorn
@@ -59,7 +61,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
     
 #Paths
@@ -73,24 +75,34 @@ class Tweet(BaseModel):
   summary="Register a User",
   tags=["Users"]
 )
-def signup():
+def signup(user: UserRegister = Body(...)):
   """"
   Signup
 
   This Path operation register a user in the app
-  
+    
   Parameters:
     -Request body parameter:
       - user: UserRegister
 
-  Returns a json with the basic user information
+  Returns a json with the basic tweet information:
     - user_id: UUID
     - email: Emailstr
     - first_name: str
     - last_name: str
-    - birth_date: str
-  """"
+    - birth_date: datetime
 
+  """
+  with open("users.json", "r+", encoding="utf-8") as f:
+    results = json.loads(f.read())
+    user_dict = user.dict()
+    user_dict["user_id"] = str(user_dict["user_id"])
+    user_dict["birth_date"] = str(user_dict["birth_date"])
+    results.append(user_dict)
+    f.seek(0)
+    f.write(json.dumps(results))
+
+    return user
 
 @app.post(
   path="/login",
@@ -110,7 +122,23 @@ def login():
   tags=["Users"]
 )
 def show_all_users():
-  pass
+  """
+  This path operation shows all users in the app
+
+  Parameters:
+    -
+  
+  Returns a JSON list with all users in the app, with the following keys:
+    - user_id: UUID
+    - email: Emailstr
+    - first_name: str
+    - last_name: str
+    - birth_date: datetime
+  """
+
+  with open("users.json", "r", encoding="utf-8") as f:
+    results = json.loads(f.read())
+    return results
 
 @app.get(
   path="/users/{user_id}",
@@ -161,8 +189,37 @@ def home():
   summary="Post a tweet",
   tags=["Tweets"]
 )
-def post():
-  pass
+def post(tweet: Tweet = Body(...)):
+  """"
+  Post a Tweet
+
+  This Path operation publish a tweet in the app
+    
+  Parameters:
+    -Request body parameter:
+      - tweet: Tweet
+
+  Returns a json with the basic tweet information:
+    - tweet_id: UUID
+    - content: str
+    - created_at: datetime
+    - updated_at: Optional[datetime]
+    - by: User
+  """
+  with open("tweets.json", "r+", encoding="utf-8") as f:
+    results = json.loads(f.read())
+    tweet_dict = tweet.dict()
+    tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+    tweet_dict["created_at"] = str(tweet_dict["created_at"])
+    tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+    tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+    tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+    results.append(tweet_dict)
+    f.seek(0)
+    f.write(json.dumps(results))
+
+    return tweet
 
 @app.get(
   path="/tweets/{tweet_id}",
